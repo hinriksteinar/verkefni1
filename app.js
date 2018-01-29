9f289c3b4ab11203ca168638c0a66ebbbc79e1b7
Hinrik <hsv7@hi.is>
2018-01-29 14:43:55 +0000

    uppfært

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express();
 const hostname = '127.0.0.1';
 const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send("Hello world!");
+});
+
 app.listen(port, hostname, () => {
   console.info(`Server running at http://${hostname}:${port}/`);
 });
---
+++ md2html.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+
+const filename = batman-ipsum.md;
+const filename = corporate-ipsum.md;
+const filename = deloren-ipsum.md;
+const filename = lorem-ipsum.md;
